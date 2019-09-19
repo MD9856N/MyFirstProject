95c47256bb0c87a85aba2b37f5b54b6e34c4566e
18322 <18322@192.168.0.167>
2019-09-18 21:08:44 -0500

    "My First Commit"

---
+++ 4010-Application/src/main/java/model/Hello.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Hello {
+
+	
+	
+	public static void main(String args[]){
+
+		Names names= new Names();
+		
+		names.setfName("Vineeth");
+		names.setlName("Reddy");
+		names.setmName("xyz");
+		names.setAge(20);
+		names.setId(1);
+		
+		System.out.println("The first Name is :" +names.getfName());
+		
+		}
+}
---
+++ 4010-Application/src/main/java/model/Names.java
@@ -0,0 +1,52 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Names {
+	
+	private String fName;
+	private String lName;
+	private String mName;
+	private int age;
+	private int id;
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+}
